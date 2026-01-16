6c5f4f3ddb97424a4fa9de07f085fc6f3573d140
gee.chris@prophecy.io <gee.chris@prophecy.io>
2026-01-16 15:25:17 +0000

    Update `Copper_Delta_Table` schema and change table reference to `chris_demos.demos.copper_delta_table` in pipelines

--- pipelines/test3_cg/code/tao_incremental_ingestion_csv/graph/Full_Load_for_Inc/Copper_Delta_Table_1.py
+++ pipelines/test3_cg/code/tao_incremental_ingestion_csv/graph/Full_Load_for_Inc/Copper_Delta_Table_1.py
@@ -15,4 +15,4 @@ def Copper_Delta_Table_1(spark: SparkSession, in0: DataFrame):
           f"abfss://{Config.var_copper_container}@{Config.var_delta_storage_account}.dfs.core.windows.net/{Config.bronze_table}"
         )\
         .mode("overwrite")\
-        .saveAsTable("`hrdp_catalog_dev`.`copper`.`copper_delta_table`")
+        .saveAsTable("`chris_demos`.`demos`.`copper_delta_table`")
